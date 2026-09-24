38fd556ba1994e544ff507d26f4312cbb1bc5734
virginia-wandii <virginia.wandii@student.moringaschool.com>
2026-09-25 00:16:45 +0300

    add test your functions

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+function calculateTax(price,taxRates) {
+    return price (taxRates/100);
 
+}
+
+
+function convertToUpperCase(string) {
+	return string.toUpperCase();
+}
+
+function findMaximum(num1,num2) {
+	return Math.max(num1,num2);
+}
+
+function isPalindrome(word) {
+	const reversed = word.split('').reverse().join('');
+	return word === reversed;
+}
+
+function calculateDiscountedPrice(price, discountPercentage) {
+	return price  (1 - discountPercentage / 100);
+}
 
 
 
